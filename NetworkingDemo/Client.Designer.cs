e4063af2f8fafc7f1a0e4ba570c986067bfbac0d
Ahmed El Araby <ahmad.met.5s@gmail.com>
2023-01-24 02:51:47 -0800

    Add project files.

---
+++ NetworkingDemo/Client.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace NetworkingDemo
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ip_txtb = new System.Windows.Forms.TextBox();
+            this.port_txtb = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.global_ip_lab = new System.Windows.Forms.Label();
+            this.local_ip_lab = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Other IP Address :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port : ";
+            // 
+            // ip_txtb
+            // 
+            this.ip_txtb.Location = new System.Drawing.Point(127, 126);
+            this.ip_txtb.Name = "ip_txtb";
+            this.ip_txtb.Size = new System.Drawing.Size(130, 20);
+            this.ip_txtb.TabIndex = 2;
+            // 
+            // port_txtb
+            // 
+            this.port_txtb.Location = new System.Drawing.Point(127, 155);
+            this.port_txtb.Name = "port_txtb";
+            this.port_txtb.Size = new System.Drawing.Size(130, 20);
+            this.port_txtb.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 212);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(227, 59);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Global IP Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "LAN IP Address :";
+            // 
+            // global_ip_lab
+            // 
+            this.global_ip_lab.AutoSize = true;
+            this.global_ip_lab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.global_ip_lab.Location = new System.Drawing.Point(124, 47);
+            this.global_ip_lab.Name = "global_ip_lab";
+            this.global_ip_lab.Size = new System.Drawing.Size(94, 24);
+            this.global_ip_lab.TabIndex = 6;
+            this.global_ip_lab.Text = "127.0.0.1";
+            // 
+            // local_ip_lab
+            // 
+            this.local_ip_lab.AutoSize = true;
+            this.local_ip_lab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.local_ip_lab.Location = new System.Drawing.Point(124, 17);
+            this.local_ip_lab.Name = "local_ip_lab";
+            this.local_ip_lab.Size = new System.Drawing.Size(94, 24);
+            this.local_ip_lab.TabIndex = 7;
+            this.local_ip_lab.Text = "127.0.0.1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(363, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 277);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 299);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.global_ip_lab);
+            this.Controls.Add(this.local_ip_lab);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.port_txtb);
+            this.Controls.Add(this.ip_txtb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ip_txtb;
+        private System.Windows.Forms.TextBox port_txtb;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label global_ip_lab;
+        private System.Windows.Forms.Label local_ip_lab;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ NetworkingDemo/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace NetworkingDemo
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            GetNetworkInfo();
+        }
+        public void GetNetworkInfo()
+        {
+            //***    this block for get local ip address   *****
+            //**************************************************
+            string hostName = Dns.GetHostName();
+           
+            // Get the IP from GetHostByName method of dns class.
+            string IP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            local_ip_lab.Text= IP;
+
+
+            //this block for get local ip address
+            var request = (HttpWebRequest)WebRequest.Create("http://ifconfig.me");
+
+            request.UserAgent = "curl"; // this will tell the server to return the information as if the request was made by the linux "curl" command
+
+            string publicIPAddress;
+
+            request.Method = "GET";
+            using (WebResponse response = request.GetResponse())
+            {
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    global_ip_lab.Text = reader.ReadToEnd();
+                }
+            }
+       
+
+        }
+        public void PingLocalDeviceWithIpAddress(string IPAddress)
+        {
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            System.Net.NetworkInformation.PingReply rep = p.Send(IPAddress);
+            if (rep.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                listBox1.Items.Add(rep.Address.ToString());
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient thisDevice = new TcpClient();
+            NetworkStream OtherDeviceStream;
+            string RecivedData ="";
+            thisDevice.Connect(port_txtb.Text.Trim(), Int32.Parse(ip_txtb.Text));
+
+           // Thread ctThread = new Thread(GetMessage());
+
+        }
+
+        public string GetMessage()
+        {
+            while(true)
+            {
+                
+            }
+            return "";
+            
+        }
+    }
+}
